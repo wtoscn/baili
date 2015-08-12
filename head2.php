c3d63629fe49cf1a88f37383dcb24f6d2032d13c
wtoscn <224409900@qq.com>
2015-08-12 22:03:29 +0800

    制作head页头模块
    hover有问题

---
+++ head2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="style/style2.css">
+</head>
+<body>
+    <div id="head">
+        <div id="head_box">
+            <div id="head_box_logo">
+                <img src="images/logo.jpg" alt="logo">
+            </div>
+            <div id="head_box_nav">
+                <ul>
+                    <li><a href="#">关于百利</a></li>
+                                <img src="images/pro_lei_1.png" alt="">
