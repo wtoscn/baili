f1add82e3ee368ecbcaac57809018d0e0839f60b
wtoscn <224409900@qq.com>
2015-08-12 22:04:17 +0800

    12

--- head2.php
+++ head2.php
@@ -14,4 +14,42 @@
             <div id="head_box_nav">
                 <ul>
                     <li><a href="#">关于百利</a></li>
+                    <li id="head_box_nav_product_li"><a href="#">商用产品</a>
+                        <div id="head_box_nav_product">
+                            <div class="head_box_nav_product_box">
                                 <img src="images/pro_lei_1.png" alt="">
+                                <a href="#">立式展示柜</a>
+                            </div>
+                            <div class="head_box_nav_product_box">
+                                <img src="images/pro_lei_2.png" alt="">
+                                <a href="#">卧式冷柜</a>
+                            </div>
+                            <div class="head_box_nav_product_box">
+                                <img src="images/pro_lei_3.png" alt="">
+                                <a href="#">岛式冷柜</a>
+                            </div>
+                            <div class="head_box_nav_product_box">
+                                <img src="images/pro_lei_4.png" alt="">
+                                <a href="#">不锈钢厨房柜</a>
+                            </div>
+                            <div class="head_box_nav_product_box">
+                                <img src="images/pro_lei_5.png" alt="">
+                                <a href="#">不锈钢工作台</a>
+                            </div>
+                            <div class="head_box_nav_product_box">
+                                <img src="images/pro_lei_6.png" alt="">
+                                <a href="#">顶盖门冷柜</a>
+                            </div>
+                        </div>
+                    </li>
+                    <li><a href="#">解决方案</a></li>
+                    <li><a href="#">服务支持</a></li>
+                    <li><a href="#">人才招聘</a></li>
+                    <li><a href="#">联系我们</a></li>
+                </ul>
+            </div>
+            <div id="head_box_ser"></div>
+        </div>
+    </div>
+</body>
+</html>
