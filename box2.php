6cff83f05b6549903dcc61078c3fd8215f1a5aad
wtoscn <224409900@qq.com>
2015-08-31 09:40:41 +0800

    优化代码

--- box2.php
+++ box2.php
@@ -3,8 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <link rel="stylesheet" href="style/box.css" type="text/css">
 </head>
 <body>
-    <img src="" alt="">
+    <div id="box_bg">
+        <div id="box">
+            <div id="box1">
+                <img src="/images/box1.jpg" alt="box1">
+                <p class="t1">百利专卖店</p><br>
+                <p  class="t2">与百利一起打造制冷行业市场</p>
+                <p  class="t2">百利专卖店面向全国招商</p>
+                <p class="t3"><a href="http://player.youku.com/player.php/sid/XODUwODQwNDk2/v.swf" allowFullScreen="true" quality="high" width="480" height="400" align="middle" allowScriptAccess="always" type="application/x-shockwave-flash">>宣传视频</a></p>
+                <p class="t3">>了解更多</p>
+            </div>
+            <div id="box2">
+                <img src="/images/box2.jpg" alt="box1">
+                <p class="t1">售后服务</p><br>
+                <p  class="t2">全年24小时在线客服</p>
+                <p  class="t2">服务网点遍布全国</p>
+            </div>
+            <div id="box3">
+                <img src="/images/box3.jpg" alt="box1">
+                <p class="t1">深度定制</p><br>
+                <p  class="t2">我们为全球千万企业商家定制</p>
+                <p  class="t2">个性化的配置服务</p>
+            </div>
+        </div>
+        <div class="clear"></div>
+    </div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>东莞市百利电器有限公司</title>
+    <link rel="stylesheet" href="style/index.css">
 </head>
 <body>
 <!--头部代码开始-->
@@ -12,7 +13,7 @@
 <?php include 'ad2.php';?>
 <!--滚动广告结束-->
 <!--3个box开始-->
-<?php include 'box.php';?>
+<?php include 'box2.php';?>
 <!--3个box结束-->
 <!--底部导航信息开始-->
 <!--底部导航信息结束-->
