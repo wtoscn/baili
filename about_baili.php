4da3f9f0eaaba0733ecd3b21e99791abdd6b9a44
wtoscn <224409900@qq.com>
2015-09-06 09:18:35 +0800

    使用阴影属性＋img标签自适应
    css3 box-shadow 为DIV添加阴影
    
    用css3 box-shadow属性 实现阴影模糊效果
    
    box-shadow:此属性包含有6个值。
    
    example:
    
     box-shadow:10px 10px 5px 10px #ccc inset；
    
    第1个值：水平阴影的长度，此值可以为负值。
    
    第2个值：垂直阴影的长度，此值可以为负值。
    
    第3个值：阴影的模糊值，此值不允许为负值。
    
    第4个值：水平阴影的外延值，此值可以为负值。
    
    第5个值：阴影的颜色。
    
    第6个值：此值为空，默认为外阴影，设置成inset为内阴影。
    ～～～～～～～～～～～～～～～～

--- about_baili.php
+++ about_baili.php
@@ -10,7 +10,22 @@
     <div class="about_ad">
         <img src="images/bg_about.jpg" alt="">
     </div>
-    <div id="about_page"></div>
+    <div id="about_page">
+        <div class="about_nav">
+            <div class="about_nav_logo">
+                <div class="color1"></div>
+                <div class="color2">
+                    <h3>关于百利</h3>
+                </div>
+            </div>
+                <ul class="about_nav_ul_bg">
+                    <li>公司简介</li>
+                    <li>公司荣誉</li>
+                    <li>百利专卖店</li>
+                </ul>
+        </div>
+        <div id="about_item"></div>
+    </div>
     
 </body>
 </html>
